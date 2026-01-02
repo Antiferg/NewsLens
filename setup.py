1b576a5c0f37d4e19a117b2a73efa5fba74cf657
Antiferg <samyarajghosh1@gmail.com>
2026-01-02 16:43:40 +0530

    packages

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    REM="-e ."
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[i.replace("\n","") for i in requirements]
+
+        if REM in requirements:
+            requirements.remove(REM)
+setup(
+    name='NewsLens',
+    version='0.0.1',
+    author='Antiferg',
+    author_email='samyarajghosh1@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
